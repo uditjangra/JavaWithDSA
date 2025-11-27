1ebc7a46fd2cacf0e754fa9d63b978827e055b41
uditjangra <uditjangrax@gmail.com>
2025-11-28 01:12:33 +0530

    init commit

--- src/LargestAndSmallestNumber.java
+++ src/LargestAndSmallestNumber.java
@@ -1,42 +1,34 @@
-import java.util.Scanner;
-
-public class LargestAndSmallestNumber {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the first number:");
-        int first = scanner.nextInt();
-        System.out.println("Enter the second number:");
-        int second = scanner.nextInt();
-        System.out.println("Enter the third number:");
-        int third = scanner.nextInt();
-        int largest = largest(first,second,third);
-        int smallest = smallest(first,second,third);
-        System.out.printf("The largest of %d ,%d and %d is %d", first, second, third, largest);
-        System.out.printf("\nThe smallest of %d ,%d and %d is %d", first,  second, third, smallest);
+public static void main(String[] ignoredArgs) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the first number:");
+    int first = scanner.nextInt();
+    System.out.println("Enter the second number:");
+    int second = scanner.nextInt();
+    System.out.println("Enter the third number:");
+    int third = scanner.nextInt();
+    int largest = largest(first, second, third);
+    int smallest = smallest(first, second, third);
+    System.out.printf("The largest of %d ,%d and %d is %d", first, second, third, largest);
+    System.out.printf("\nThe smallest of %d ,%d and %d is %d", first, second, third, smallest);
+}
+public static int largest(int first, int second, int third) {
+    int max = first;
+    if (max < second) {
+        max = second;
     }
-    public static int largest(int first, int second, int third) {
-        int max = first;
-        if(max < second){
-            max = second;
-        }
-        if(second < third){
-            max = third;
-        }
-        return max;
+    if (second < third) {
+        max = third;
     }
-
-    public static int smallest(int first, int second, int third) {
-        int min = first;
-        if (second < min) {
-            min = second;
-        }
-
-        if (third < min) {
-            min = third;
-        }
-
-        return min;
+    return max;
+}
+public static int smallest(int first, int second, int third) {
+    int min = first;
+    if (second < min) {
+        min = second;
     }
-
+    if (third < min) {
+        min = third;
+    }
+    return min;
 }
 
